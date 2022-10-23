f991aa2891ade3ebdf5d15c653937857e84f44cd
Santiago Paeres <s.paeres10@uniandes.edu.co>
2022-10-23 04:52:41 -0500

    Funciona transferir archivos UDP

--- CLIENTE/cliente.py
+++ CLIENTE/cliente.py
@@ -25,20 +25,22 @@ def escribir_directorio():
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-def escribir_log(x,y,tipo_archivo,exito, hash):
-    nombre_log = "Cliente"+x+"-Prueba-"+y+"("+tipo_archivo+").txt"
-    file = open("./ArchivosRecibidos/"+nombre_log,"a")
+
+
+def escribir_log(x, y, tipo_archivo, exito, hash):
+    nombre_log = "Cliente" + x + "-Prueba-" + y + "(" + tipo_archivo + ").txt"
+    file = open("./ArchivosRecibidos/" + nombre_log, "a")
     file.write("------------Recibido-------------\n")
-    if(tipo_archivo=="100MB"):
-        file.write("Archivo recibido:   "+NOMBRE_ARCHIVO_100M + " de tamaño 100MB\n")
-    elif(tipo_archivo=="250MB"):
-        file.write("Archivo recibido:   "+NOMBRE_ARCHIVO_250M + " de tamaño 250MB\n")
-    if(exito):
+    if (tipo_archivo == "100MB"):
+        file.write("Archivo recibido:   " + NOMBRE_ARCHIVO_100M + " de tamaño 100MB\n")
+    elif (tipo_archivo == "250MB"):
+        file.write("Archivo recibido:   " + NOMBRE_ARCHIVO_250M + " de tamaño 250MB\n")
+    if (exito):
         file.write("El hash recibido es igual al hash del archivo recibido\n")
     else:
         file.write("El hash recibido es diferente al hash del archivo recibido\n")
 
-    file.write("hash esperado: "+hash)
+    file.write("hash esperado: " + hash)
     file.close()
 
 
@@ -53,12 +55,16 @@ def comparar_hash(archivo_recibido, hash_recibido):
     md5_hash.update(content)
 
     digest = md5_hash.hexdigest()
-    if(digest == hash_recibido):
+    if (digest == hash_recibido):
         iguales = True
     return iguales, digest
 
 
-# Create a TCP/IP socket
+def verificar_transferencia_exitosa(numero_paquetes_recibidos, numero_paquetes_esperados):
+    return numero_paquetes_recibidos == numero_paquetes_esperados
+
+
+# Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Connect the socket to the port on the server
@@ -73,14 +79,16 @@ sock.connect(server_address)
 escribir_directorio()
 
 try:
-    entrada0= input('¿Cuantos clientes desea? (Minimo 1 - maximo 25)\n')
+    entrada0 = input('¿Cuantos clientes desea? (Minimo 1 - maximo 25)\n')
     for i in range(int(entrada0)):
-        message = b'Hola, estoy listo para recibir archivos |'+bytes(sock.getsockname()[0], 'ascii')+b"|"+bytes(str(sock.getsockname()[1]), 'ascii')
+        message = b'Hola, estoy listo para recibir archivos |' + bytes(sock.getsockname()[0], 'ascii') + b"|" + bytes(
+            str(sock.getsockname()[1]), 'ascii')
         sock.send(message)
         dataInicial = sock.recv(1024)
         print('Recibi tu mensaje: {!r}'.format(dataInicial))
 
-        entrada1 = input('Que archivo quieres? 100 MB o 250 MB? (Por favor escribir unicamente el que desea: Ej: "100MB")\n')
+        entrada1 = input(
+            'Que archivo quieres? 100 MB o 250 MB? (Por favor escribir unicamente el que desea: Ej: "100MB")\n')
         rutaDeseada = ''
         ruta_archivo = ''
         nombre_archivo = ''
@@ -103,7 +111,7 @@ try:
             datosArchivoRestantes = recibido[recibido.index('|') + 1:len(recibido)]
         else:
             raise Exception('Deberia recibir el hash')
-            
+
         print('Hash recibido:' + nombre)
         # Archivo:
         f = open(ruta_archivo, 'wb')
@@ -123,12 +131,11 @@ try:
         print('Recibido: ', despedida)
         print('Verificando integridad del archivo....')
         comparacion = comparar_hash(nombre_archivo, nombre)
-        print('Hash verificado correctamente!') if comparacion[0] else print('Hash enviado por el servidor no es correcto:'+nombre+"!="+comparacion[1])
-        x=i+1
-        escribir_log(str(x),entrada0,entrada1,comparacion[0],comparacion[1])
-        print('Recibido despedida server: '+despedida)
+        print('Hash verificado correctamente!') if comparacion[0] else print(
+            'Hash enviado por el servidor no es correcto:' + nombre + "!=" + comparacion[1])
+        x = i + 1
+        escribir_log(str(x), entrada0, entrada1, comparacion[0], comparacion[1])
+        print('Verificando transferencia exitosa....')
+        print('Recibido despedida server: ' + despedida)
 finally:
     sock.close()
-
-
-    
